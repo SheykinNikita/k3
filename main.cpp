70c0a1af37e505bc87cafd1574849a67a489fa4b
Nikita <sheikinnikita@yandex.ru>
2018-10-05 15:40:25 +0300

    k1

--- main.cpp
+++ main.cpp
@@ -1,13 +1,6 @@
 #include <iostream>
 
 int main() {
-    long int a = 128789;
-    int b = 125;
-    int c = 32768;
-    float d = 1000.5;
-    std::cout << a << std::endl;
-    std::cout << b << std::endl;
-    std::cout << c << std::endl;
-    std::cout << d << std::endl;
+    short a = 2091;
     return 0;
 }
